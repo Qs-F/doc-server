d40777b3c6adb7cfe1e3bc7bb8fc49efb0559b9c
Qs-F <qsf@mail.queensya.com>
2016-05-30 01:16:09 +0900

    first import

---
+++ README.md
@@ -0,0 +1,11 @@
+# package doc-server
+
+A server to serve documents.
+
+## lib/doc.go
+
+Return markdown style file as converted html file.  
+Using current directory's md file.
+
+Writing on spesificted directory without any markdown file.  
+converting md to html
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"flag"
+	"github.com/russross/blackfriday"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"text/template"
+)
+
+const (
+	tmpl = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta viewport="width=device-width">
+	<meta charset="UTF-8">
+	<title>{{.Title}}</title>
+	<link rel="stylesheet" href="/style.css">
+</head>
+<body>
+{{.Content}}
+</body>
+</html>
+`
+)
+
+func main() {
+	port := flag.Int("p", 8080, "setting application port number. default is 8080")
+	flag.Parse()
+	type Page struct {
+		Title   string
+		Content string
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		wd, _ := os.Getwd()
+		if v, err := os.Stat(wd + "/" + r.URL.Path[1:]); err == nil && v.Mode().IsRegular() { // check whether requested file exist. DONOT change the order of if checks.
+			if strings.HasSuffix(r.URL.Path[1:], ".md") { // check whether is it requesting for markdown file.
+				f, err := ioutil.ReadFile(wd + "/" + r.URL.Path[1:])
+				if err != nil {
+					http.NotFound(w, r)
+					return
+				}
+				b := blackfriday.MarkdownCommon(f)
+				tmplen, err := template.New("tmpl").Parse(tmpl)
+				p := Page{
+					Title:   r.URL.Path[1:],
+					Content: string(b),
+				}
+				err = tmplen.Execute(w, p)
+				if err != nil {
+					http.NotFound(w, r)
+				}
+			} else {
+				http.ServeFile(w, r, r.URL.Path[1:])
+			}
+		} else {
+			http.ServeFile(w, r, r.URL.Path[1:])
+		}
+	})
+	http.ListenAndServe("0.0.0.0:"+strconv.Itoa(*port), nil)
+}
